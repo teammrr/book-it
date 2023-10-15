6ec79eaaab6d9b76334e9e35c42502ae9b38b9cc
Suteemon <suteemon.team@newton.ac.th>
2023-10-15 23:58:18 +0700

    clean up code

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -18,32 +18,6 @@ export default function page() {
           <h2 className="font-bold text-2xl text-[#002D74] text-center">
             Welcome Back
           </h2>
-          {/* <form className="text-sm flex text-[#002D74] flex-col gap-4 mt-4">
-            <input
-              type="email"
-              className="p-2 rounded-xl border"
-              placeholder="Email / ชื่อผู้ใช้"
-            />
-            <input
-              type="password"
-              className="p-2 rounded-xl border w-full"
-              placeholder="รหัสผ่าน"
-            />
-            <button
-              className="bg-[#002D74] text-white rounded-xl py-2 hover:scale-105
-            duration-300"
-            >
-              เข้าสู่ระบบ
-            </button>
-          </form>
-          <div className="mt-2 text-xs text-[#002D74] underline">
-            <a href="#">ลืมรหัสผ่าน</a>
-          </div>
-          <div className="mt-6 text-gray-400 grid items-center grid-cols-3">
-            <hr className="border-gray-400" />
-            <p className="text-center text-sm">หรือ</p>
-            <hr className="border-gray-400" />
-          </div> */}
           <button
             onClick={() => signIn("google")}
             className="bg-white px-2 py-2 mt-5 border w-full rounded-xl 
@@ -57,13 +31,6 @@ export default function page() {
             />
             <span className="px-2">Sign In WIth Google</span>
           </button>
-
-          {/* <div className="mt-5 text-xs flex justify-between items-center">
-            <p className="text-gray-400">คุณยังไม่มีบัญชีใช่หรือไม่?</p>
-            <a href="#" className="text-[#002D74]">
-              สมัครสมาชิก
-            </a>
-          </div> */}
         </div>
       </div>
     </section>
