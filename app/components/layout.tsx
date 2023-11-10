10abdf318591b63d9146c098f01c73d0f632466c
Suteemon <suteemon.team@newton.ac.th>
2023-11-10 09:16:30 +0700

    added header animation on scroll

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -11,16 +11,18 @@ const variants = {
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
-      <Header />
+      <motion.div className="sticky top-0 z-50">
+        <Header />
+      </motion.div>
       <motion.main
         variants={variants}
         initial="hidden"
         animate="enter"
-        transition={{ type: "linear" }}
+        transition={{ type: "spring", bounce: 0.5 }}
       >
         <main className="bg-gray-50 text-black min-h-max">{children}</main>
+        <Footer />
       </motion.main>
-      <Footer />
     </>
   );
 }
